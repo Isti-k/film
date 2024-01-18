4f69bb7c67f1dc8ca8788a0afe5ce2aba9648e19
Isti-k <kiscsere03@gmail.com>
2024-01-18 09:22:00 +0100

    befejez

--- Faljbeolvas.py
+++ Faljbeolvas.py
@@ -29,11 +29,37 @@ def legrovidebb_film(Film_lista):
             legrovidebb+=i
     return legrovidebb
 
-
+def legalabb110p(Film_lista):
+    db:int=0
+    for i in range(0,len(Film_lista),1):
+        if Film_lista[i].perc >= 110:
+            db += 1
+    return db
         
+def filmbeker(Film_lista):
+    nev:str=str(input("Kérek egy színész nevet!"))
+    van:int=0
+    for i in range(0,len(Film_lista),1):
+        if Film_lista[i].főszereplő == nev:
+            print(f"Ajánlom ezt a filmet:{Film_lista[i].cím}")
+            van = 1
+    if van == 0:
+        print(f"Nincs ilyen Színész!")
+
+    return nev
 
 
-
+def falj_kiir(Film_lista,nev):
+    fajl=open("film2.txt","w",encoding="UTF-8")
+    
+    van:int=0
+    for i in range(0,len(Film_lista),1):
+        if Film_lista[i].főszereplő == nev:
+            fajl.write(f"Ajánlom ezt a filmet:{Film_lista[i].cím}")
+            van = 1
+    if van == 0:
+        fajl.write(f"Nincs ilyen Színész!")
+    fajl.close()
 
 
 
--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import Faljbeolvas
 
 lista=Faljbeolvas.faljbeolvasas()
 legrov=Faljbeolvas.legrovidebb_film(lista)
-print(f"A legrövidebb film:{legrov}")
-
+print(f"A legrövidebb film:{lista[legrov].cím}")
+legalabb=Faljbeolvas.legalabb110p(lista)
+print(f"Legalább {legalabb}db 110p-es film van.")
+film=Faljbeolvas.filmbeker(lista)
+print(film)
+Faljbeolvas.falj_kiir(lista,film)
 
 
 
